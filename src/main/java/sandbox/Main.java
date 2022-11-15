98ab852a86d6b3f915697f6dab942967769db34b
eckert.tom <toe00@web.de>
2022-11-15 16:53:05 +0100

    test casting

---
+++ src/main/java/sandbox/Main.java
@@ -0,0 +1,9 @@
+package sandbox;
+
+public class Main {
+    public static void main(String[] args) {
+        char c = '1';
+        double d = ((double) c);
+        System.out.println(c);
+    }
+}
